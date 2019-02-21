871f182878f1a67c626c75f344bd298e4f5877a6
spardadanthe <stanimir.venkov@eldersoss.com>
2019-02-21 15:39:31 +0200

    Add models

---
+++ Chatman/Conversation.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Chatman
+{
+    public class Conversation
+    {
+        public List<User> UsersParticipating { get; set; }
+        public List<Message>  History { get; set; }
+}
+}
---
+++ Chatman/Message.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Chatman
+{
+    public class Message
+    {
+        public Message()
+        {
+            Date  = DateTime.Now;
+        }
+
+        public User Sender { get; set; }
+        public string Text { get; set; }
+        DateTime Date { get; set; }
+    }
+}
