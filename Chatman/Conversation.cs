65d01c258e86630ba19d2b4c9c8871ca9c574989
spardadanthe <stanimir.venkov@eldersoss.com>
2019-02-25 11:31:15 +0200

    Implement Get/Add  Conversations

--- Chatman/Conversation.cs
+++ Chatman/Conversation.cs
@@ -4,6 +4,11 @@ namespace Chatman
 {
     public class Conversation
     {
+        public Conversation()
+        {
+            UsersParticipating = new List<User>();
+            History = new List<Message>();
+        }
         public List<User> UsersParticipating { get; set; }
         public List<Message>  History { get; set; }
 }
