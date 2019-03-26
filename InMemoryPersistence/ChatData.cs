5bb0f4dbe641f76ecbe5a8783e75b418141bf748
spardadanthe <stanimir.venkov@eldersoss.com>
2019-03-26 15:41:53 +0200

    Add InMemory Persistence

---
+++ InMemoryPersistence/ChatData.cs
@@ -0,0 +1,13 @@
+﻿using Chatman;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InMemoryPersistence
+{
+    public static class ChatData
+    {
+        public static ICollection<User> Users = new HashSet<User>();
+        public static ICollection<Conversation> Conversations = new HashSet<Conversation>();
+    }
+}
---
+++ InMemoryPersistence/Repositories/UserRepository.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Chatman;
+using Persistence.Services;
+
+namespace InMemoryPersistence.Repositories
+{
+    public class InMemoryUserRepository : IRepository<User>
+    {
+        private readonly ICollection<User> users;
+
+        public InMemoryUserRepository()
+        {
+            users = ChatData.Users;
+        }
+
+        public ICollection<User> GetAll()
+        {
+            return users;
+        }
+
+        public void Add(User user)
+        {
+            users.Add(user);
+        }
+    }
+}
