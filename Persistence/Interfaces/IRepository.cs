35d6d60fefbf9c90909203dca63eadf3f66117db
spardadanthe <stanimir.venkov@eldersoss.com>
2019-03-27 16:44:16 +0200

    Remove unnecessary files from Persistence & InMemoryPersistence

--- Persistence/Interfaces/IRepository.cs
+++ Persistence/Interfaces/IRepository.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using Chatman;
+using System.Collections.Generic;
 
-namespace Persistence.Services
+namespace Persistence.Interfaces
 {
     public interface IRepository<T> where T : class
     {
         ICollection<T> GetAll();
+
+        T GetById(BaseId id);
 
         void Add(T entity);
     }
