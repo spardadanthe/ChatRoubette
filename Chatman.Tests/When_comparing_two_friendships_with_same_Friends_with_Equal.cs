de4c43c1982d14a421999d10bf65ea0e4fee55dc
spardadanthe <stanimir.venkov@eldersoss.com>
2019-03-27 16:42:59 +0200

    Write tests for comparing friendships
    -with Equals
    -with == and ~!= operators

---
+++ Chatman.Tests/When_comparing_two_friendships_with_same_Friends_with_Equal.cs
@@ -0,0 +1,31 @@
+﻿using Machine.Specifications;
+
+namespace Chatman.Tests
+{
+    [Subject("Friendship")]
+    public class When_comparing_two_friendships_with_same_Friends_with_Equal
+    {
+
+        Establish context = () =>
+        {
+            user1 = new User("Vanko");
+            user2 = new User("Goshko");
+            firstFriendship = new Friendship((UserId)user1.Id, (UserId)user2.Id);
+            secondFriendship = new Friendship((UserId)user2.Id, (UserId)user1.Id);
+            areEqual = false; 
+        };
+
+        Because of = () =>
+        {
+            areEqual = firstFriendship.Equals(secondFriendship);
+        };
+
+        It should_be_the_same = () => areEqual.ShouldBeTrue();
+
+        public static bool areEqual;
+        public static User user1;
+        public static User user2;
+        public static Friendship firstFriendship;
+        public static Friendship secondFriendship;
+    }
+}
