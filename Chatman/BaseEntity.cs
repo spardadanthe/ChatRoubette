14318674f23fd96c840a2af961d0225571c525dd
spardadanthe <stanimir.venkov@eldersoss.com>
2019-03-27 16:41:16 +0200

    Added BaseId and BaseEntity

---
+++ Chatman/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chatman
+{
+    public class BaseEntity
+    {
+        public BaseEntity(BaseId id)
+        {
+            Id = id ?? throw new ArgumentNullException(nameof(id));
+        }
+
+        public BaseId Id { get; private set; }
+    }
+}
---
+++ Chatman/BaseId.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chatman
+{
+    public class BaseId
+    {
+        public BaseId(string value)
+        {
+            if (string.IsNullOrEmpty(value)) new ArgumentException(nameof(value));
+            Value = value;
+        }
+
+        public string Value { get; private set; }
+
+        public override bool Equals(object obj)
+        {
+            return Value == ((BaseId)obj).Value;
+        }
+
+        public static bool operator ==(BaseId b1, BaseId b2)
+        {
+            if (b1.Value == b2.Value) return true;
+
+            return false;
+        }
+
+        public static bool operator !=(BaseId b1, BaseId b2)
+        {
+            if (b1.Value != b2.Value) return true;
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
+
+    }
+}
