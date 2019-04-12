b8b430a82b2f4e75df3201d9b9e1958cf7385d0c
spardadanthe <stanimir.venkov@eldersoss.com>
2019-04-12 17:20:26 +0300

    Add ConversationResponseModel

--- Chatman.Api/Controllers/ConversationsController.cs
+++ Chatman.Api/Controllers/ConversationsController.cs
@@ -19,29 +19,32 @@ namespace Chatman.Api.Controllers
         }
 
         [HttpGet]
-        public ActionResult<ICollection<Conversation>> Get()
+        public ActionResult<ICollection<ConversationResponseModel>> Get()
         {
             ICollection<Conversation> allConversations = convRepository.GetAll();
             if (allConversations is null) return NotFound("There are currently no conversations");
 
-            ICollection<ConversationResponseModel> response = ConvResponseModelConverter(allConversations);
+            ICollection<ConversationResponseModel> response = ListConvResponseModelConverter(allConversations);
 
             return Ok(response);
         }
 
         [HttpGet]
         [Route("{id}")]
-        public ActionResult<Conversation> GetById(string id)
+        public ActionResult<ConversationResponseModel> GetById(string id)
         {
-            //var conversation = convRepository.GetAll().SingleOrDefault(x => x.Id.Value == id);
             var conversation = convRepository.GetById(new BaseId(id));
 
-            return Ok(conversation);
+            
+            
+            ConversationResponseModel response = ConvResponseModelConverter(conversation);
+
+            return Ok(response);
         }
 
         [HttpPost]
         public ActionResult Add(ConversationRequestModel conv)
-       {
+        {
             var convId = new ConversationId(conv.Id);
             var userIds = new List<UserId>();
             var ownerId = new UserId(conv.OwnerId);
@@ -54,7 +57,7 @@ namespace Chatman.Api.Controllers
                 usersParticipatingIds.Add(userParticipatingId);
             }
 
-            Conversation convToBeAdded = new Conversation(convId, usersParticipatingIds,ownerId);
+            Conversation convToBeAdded = new Conversation(convId, usersParticipatingIds, ownerId);
 
             bool found = convRepository.GetAll().Any(x => x.Id == convToBeAdded.Id);
             if (found) return Ok();
@@ -63,22 +66,40 @@ namespace Chatman.Api.Controllers
             return Ok();
         }
 
-        private ICollection<ConversationResponseModel> ConvResponseModelConverter(ICollection<Conversation> allConversations)
+        private ICollection<ConversationResponseModel> ListConvResponseModelConverter(ICollection<Conversation> allConversations)
         {
             var conversationResponseModels = new List<ConversationResponseModel>();
 
             foreach (var conversation in allConversations)
             {
-                var blockedUsersIds = conversation.BlockedUsersIds.Select(x=>x.Value);
-                var usersParticipatingIds = conversation.BlockedUsersIds.Select(x => x.Value);
+                ConversationResponseModel conversationResponseModel = ConvResponseModelConverter(conversation);
                 
-                var conversationResponseModel = new ConversationResponseModel(conversation.Id.Value,
-                    conversation.OwnerId.Value, blockedUsersIds, usersParticipatingIds);
-
                 conversationResponseModels.Add(conversationResponseModel);
             }
 
             return conversationResponseModels;
+        }
+
+        private ConversationResponseModel ConvResponseModelConverter(Conversation conversation)
+        {
+            IEnumerable<string> blockedUsersIds = null;
+            IEnumerable<string> usersParticipatingIds = null;
+
+            if (conversation.BlockedUsersIds is null == false)
+            {
+                blockedUsersIds = conversation.BlockedUsersIds.Select(x => x.Value);
+            }
+
+            if (conversation.UsersParticipatingIds is null == false)
+            {
+                usersParticipatingIds = conversation.BlockedUsersIds.Select(x => x.Value);
+            }
+
+            var conversationResponseModel = new ConversationResponseModel(conversation.Id.Value,
+                conversation.OwnerId.Value, blockedUsersIds, usersParticipatingIds);
+
+            return conversationResponseModel;
+
         }
     }
 }
---
+++ Chatman.Api/ResponseModels/ConversationResponseModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Chatman.Api.ResponseModels
+{
+    public class ConversationResponseModel
+    {
+        public ConversationResponseModel(string id,string ownerId,
+            IEnumerable<string> blockedUsersIds, IEnumerable<string> usersparticipatingIds)
+        {
+            Id = id;
+            OwnerId = ownerId;
+            BlockedUsersIds = blockedUsersIds;
+            UsersParticipatingIds = usersparticipatingIds;
+        }
+
+        public string Id { get; private set; }
+        public string OwnerId { get; private set; }
+        public IEnumerable<string> BlockedUsersIds { get; private set; }
+        public IEnumerable<string> UsersParticipatingIds { get; private set; }
+    }
+}
